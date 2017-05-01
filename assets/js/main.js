7ccb2ff32bf874769fde949620673c89e6512c90
hockey4life63 <Amelancon68@gmail.com>
2017-04-30 21:57:49 -0700

    started to add images and style

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,9 @@
 //list of all character
 var charList = [
-	["name1",5,"name1.jpg","namw1.wmv", 100],
-	["name2",10,"name2.jpg","namw2.wmv", 110],
-	["name3",15,"name3.jpg","namw3.wmv", 150],
-	["name4",5,"name4.jpg","namw4.wmv",120]
+	["korben",5,"korben-dallas.jpg","namw1.wmv", 100,"Korben Dallas"],
+	["leeloo",10,"leeloo.jpg","namw2.wmv", 110, "Leeloo"],
+	["ruby",15,"ruby-rhod.jpg","namw3.wmv", 150, "Ruby Rhod"],
+	["zorg",5,"zorg.jpg","zorg.wav",120, "Jean-Baptiste Zorg"]
 	];
 var nameList =[];
 var user;
@@ -12,20 +12,22 @@ var charClass = "charClass";
 	function charConstuct(list){
 		//creates all the objects for the character
 		for (var i = 0; i < list.length; i++) {
-			window[list[i][0]] = new char(list[i][0],list[i][1],list[i][2],list[i][3],list[i][4]);
+			window[list[i][0]] = new char(list[i][0],list[i][1],list[i][2],list[i][3],list[i][4],list[i][5]);
 			nameList.push(list[i][0]);
 		}
+		//adds click listener
 		for (var i = 0; i < nameList.length; i++) {
 			window[nameList[i]].create();
 			$(window[nameList[i]].id).on("click",userClick.bind(window[nameList[i]]));
 		};
 	};
-
 function defenderClick(){
 	//sets defender and removes from list
 	this.delete();
 	this.defenderArea();
+	//sets as the defender
 	defender = this;
+	//removes from namList
 	let index = nameList.indexOf(this.name);
 	nameList.splice(index, 1);
 	//removes other click listners
@@ -33,6 +35,7 @@ function defenderClick(){
 		let item = window[nameList[i]];
 		$(item.id).unbind("click");
 	};
+	// add fight button click listener
 	$("#fightButton").on("click", function(){
 		user.attack(defender);
 	});
@@ -41,9 +44,9 @@ function defenderClick(){
 function userClick(){
 	//must be called with a bind() for this to work
 	//removes clicked character frm nameList and sets it user to True
+	this.sound.play();
 	$(this.id).unbind();
 	let index = nameList.indexOf(this.name);
-	this.user = true;
 	user = this;
 	nameList.splice(index,1);
 	//deletes other character boxs
@@ -71,7 +74,6 @@ function gameWin(){
 	defender.delete();
 	user.delete();
 	restartButton();
-
 }
 function gameLose(){
 	user.delete();
@@ -108,14 +110,20 @@ function combatCheck(player, target){
 var create = function(){
 	//create the box with the image and name for the char object
 	var item =$("<div>");
+	//gives it id based on its name
 	item.attr("id", this.name);
+	//adds image with charImg class
 	item.append($("<img>").attr("src",this.img).addClass("charImg"));
+	//adds own class and general charClass
 	item.addClass(this.class+ " "+charClass);
-	item.append($("<h2>").addClass("charName").html(this.name))
-	item.append($("<h2>").addClass("health").html("Health:"))
-		.append($("<div>").attr("id",this.name+"Health").html(this.health));
-	item.append($("<h2>").addClass("attack").html("attack:"))
-		.append($("<div>").attr("id",this.name+"Damage").html(this.damage));
+	//adds name
+	item.append($("<h2>").addClass("charName").html(this.displayName))
+	//add health info and ids to update it
+	item.append($("<h2>").addClass("health").html("Health:")
+		.append($("<div>").attr("id",this.name+"Health").html(this.health)));
+	//adds attack info and ids to change it
+	item.append($("<h2>").addClass("attack").html("attack:")
+		.append($("<div>").attr("id",this.name+"Damage").html(this.damage)));
 	$(this.location).append(item);
 };
 var objDelete =  function(){
@@ -123,38 +131,48 @@ var objDelete =  function(){
 	$(this.id).remove();
 };
 var enemyArea = function(){
-		
 		this.location = "#enemyArea";
 		this.create();
 		$(this.id).removeClass(this.class);
-		this.class = "defenderChoice";
+		this.class = "enemyStyle";
 		$(this.id).addClass(this.class);
 	};
 var defenderArea = function(){
 	this.location = "#defenderArea"
 	this.create();
+	$(this.id).removeClass(this.class);
+	this.class = "defenderChoice";
+	$(this.id).addClass(this.class);
 };
 var attack = function(target){
+	//applys damage
 	target.health -= this.damage;
-	this.health -= target.damage;
 	this.damage += this.attackIncrease;
+	//checks if emeny died and returns
+	if(target.health <= 0){
+		combatCheck(this, target);
+		this.updateStats();
+		return
+	}
+	//applys counter attack if enemy lives
+	this.health -= target.damage;
 	this.updateStats();
 	target.updateStats();
 	combatCheck(this, target);
-
 };
 var updateStats = function(){
 	$(this.id+"Health").html(this.health);
 	$(this.id+"Damage").html(this.damage);
 };
-function char(name, damage, img, sound, health){
+function char(name, damage, img, sound, health, displayName){
 	//object constructor
-	this.name = name;
+	this.name = name
+	this.displayName =displayName;
 	this.damage = damage;
 	this.attackIncrease =damage;
 	this.health = health
 	this.img = "assets/images/"+img;
-	this.sound = "assets/sounds/"+sound;
+	this.sound = new Audio("assets/sounds/"+sound);
 	this.location = "#userArea";
 	this.class = "userChoice";
 	this.id = "#"+name;
